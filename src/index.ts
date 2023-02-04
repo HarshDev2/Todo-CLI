f401bd17200f6525a6a1ef226b545c165392aedb
Harsh Agrawal <averageworkspace@gmail.com>
2023-02-04 09:19:16 +0530

    added add command

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,31 @@
 #!/usr/bin/env node
+import { readFile, writeFile } from 'fs/promises';
+import { existsSync } from 'fs';
+import inquirer from 'inquirer'
+
+let command = process.argv[2];
+if(command === 'add'){
+    const questions = await inquirer.prompt([
+        {
+            message: 'What should be the todo title?',
+            type: 'input',
+            name: 'title'
+        },
+    ]);
+    
+    if(!existsSync('todos.json')){
+        await writeFile('todos.json', JSON.stringify([{title: questions.title}]));
+    }
+    else{
+        const file = await readFile('todos.json', 'utf-8');
+        const data = await JSON.parse(file);
+
+        console.log(file);
+        console.log(data);
+
+        await data.push({title: questions.title});
+
+        await writeFile('todos.json', JSON.stringify(data));
+    }
+    console.log(`A todo named "${questions.title}" has been sucessfully created!`);
+}
